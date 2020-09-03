880bc2ed36c7134f530ca7688886d523d45b8d95
HARDIK GUPTA <parnami.hardik50@gmail.com>
2020-09-03 21:51:56 +0530

    Added Some Css

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ const sysDate = new Date();
 let luckyNumber=Math.floor(Math.random() * 100); 
 ReactDOM.render(
   <>
-  <h1>Hello Welcome to React!</h1>
-  <h3>{`Today is ${sysDate}`}</h3>
-  <p>{`Your Lucky Number is : ${luckyNumber}`}</p>
+  <h1 className ="heading">Hello Welcome to React!</h1>
+  <h3 id="date">{`Today is ${sysDate}`}</h3>
+  <p id = "lucynumber">{`Your Lucky Number is : ${luckyNumber}`}</p>
   <img src="https://picsum.photos/536/354" alt=""></img>
   </>,
   document.getElementById('root')
